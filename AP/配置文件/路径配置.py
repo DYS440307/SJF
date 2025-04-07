de033b0b85f3fef50fe0760697425763dbbf3979
DYS <dys440307@gmail.com>
2025-04-07 18:33:43 +0800

    c

--- AP/配置文件/路径配置.py
+++ AP/配置文件/路径配置.py
@@ -1,6 +1,6 @@
 # 实验记录文件路径
 experiment_record_path = r"\\192.168.11.24\惠州声乐\3-品质部\实验室\邓洋枢\2-实验记录汇总表\2025年\实验记录.xlsx"
 # 实验报告文件路径
-experiment_report_path = r"C:\Users\SL\Pictures\A报告\实验报告.xlsx"
+experiment_report_path = r"E:\System\pic\A报告\实验报告.xlsx"
 # IMP
-IMP_path = r"C:\Users\SL\Pictures\A报告\IMP数据.xlsx"
+IMP_path = r"E:\System\pic\A报告\IMP数据.xlsx"
